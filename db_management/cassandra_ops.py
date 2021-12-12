6491c58e910ad86fdbf852a643a066ec250f1e3e
IamVicky90 <waqasbilal02@gmail.com>
2021-12-12 19:13:23 +0500

    first commit

---
+++ db_management/cassandra_ops.py
@@ -0,0 +1,33 @@
+import os
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import pandas as pd
+class cassandra_ops:
+    def __init__(self,):
+        pass
+    def get_connection_to_cassandra_db(self,keyspace):
+        try:
+            cloud_config= {
+                    'secure_connect_bundle': 'secure-connect-health-app.zip'
+            }
+            CASSANDRA_USERNAME=os.environ.get('CASSANDRA_USERNAME')
+            CASSANDRA_Password=os.environ.get('CASSANDRA_Password')
+            auth_provider = PlainTextAuthProvider(CASSANDRA_USERNAME,CASSANDRA_Password )
+            cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+            session = cluster.connect(keyspace)
+            return cluster, session
+        except Exception as e:
+            raise e
+        return session
+    def get_tables_from_cassandra_db(self,keyspace='project_loggings',cluster=None,session=None):
+        session.execute(f'use {keyspace};')
+        tables=cluster.metadata.keyspaces['project_loggings'].tables
+        return list(tables.keys())
+    def convert_tables_into_pandas_csv(self,session,tables):
+        for table in tables:
+            df=pd.DataFrame(session.execute(f'select * from {table};'))
+            df.to_csv(os.path.join(os.getcwd(),'csv_data',f'{table}.csv'))
+cassandra_ops_obj=cassandra_ops()
+cluster,session=cassandra_ops_obj.get_connection_to_cassandra_db('project_loggings')
+tables=cassandra_ops_obj.get_tables_from_cassandra_db('project_loggings',cluster=cluster,session=session)
+cassandra_ops_obj.convert_tables_into_pandas_csv(session,tables)
