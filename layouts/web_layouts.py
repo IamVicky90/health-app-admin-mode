ac247a361dbbd87fd909fcbcd1bea83feeae8573
IamVicky90 <waqasbilal02@gmail.com>
2021-12-29 18:48:42 +0500

    remove comments from web_layouts.py file

--- layouts/web_layouts.py
+++ layouts/web_layouts.py
@@ -4,10 +4,10 @@ from dash import html
 from dash import dcc
 class create_layouts:
     def __init__(self):
-        # cassandra_ops_obj=CASSANDRRA_OPS()
-        # cluster,session=cassandra_ops_obj.get_connection_to_cassandra_db('project_loggings')
-        # tables=cassandra_ops_obj.get_tables_from_cassandra_db('project_loggings',cluster=cluster,session=session)
-        # cassandra_ops_obj.convert_tables_into_pandas_csv(session,tables)
+        cassandra_ops_obj=CASSANDRRA_OPS()
+        cluster,session=cassandra_ops_obj.get_connection_to_cassandra_db('project_loggings')
+        tables=cassandra_ops_obj.get_tables_from_cassandra_db('project_loggings',cluster=cluster,session=session)
+        cassandra_ops_obj.convert_tables_into_pandas_csv(session,tables)
         self.graph_ops_obj=Graph_ops()
     def return_app_graph(self,dash_app):
         error_occured_in_func_fig,real_error_fig=self.graph_ops_obj.create_app_graph()
